90dc696b67de2bb010ed303dfa72170252d116f8
monica82-hany <monicahanybio.com>
2024-10-30 22:15:07 +0300

    new

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 inputFile = "exampledata/aamutiline.fasta"
-from moduls_folder.moduls import fastaread,GCcount
+# from moduls_folder.moduls import fastaread,GCcount
 
+ 
+seqs={}
+with open (inputFile,"r") as f:
+        header=None
+        for line in f :
+            line=line.strip()
+            if len(line) == 0 :
+                continue
+            if line.startswith(">"):
+                header=line[1:]
+                header=line.split()[0]
+                seqs[header]=""
+                print(line)
+            else:
+                seqs[header]+=line
+# file = fastaread(inputFile)  
 
-file = fastaread(inputFile)  
-
-s= GCcount(inputFile)
-print(file)
-print(s)
+# s= GCcount(inputFile)
+# print(file)
+# print(s)
 
 
